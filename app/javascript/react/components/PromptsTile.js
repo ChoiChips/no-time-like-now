be3829a159a52b6cb25c95548518094743fd8536
ChoiChips <christianchoi2@gmail.com>
2018-05-08 15:44:39 -0400

    -Added SCSS files including partials. Needed to modify stylesheets/application.css -Added #create to Prompts API controller -Minor adjustments to PromptsTile

--- app/javascript/react/components/PromptsTile.js
+++ app/javascript/react/components/PromptsTile.js
@@ -6,11 +6,11 @@ const PromptsTile = (props) => {
   return (
     <div className="columns small-12 medium-6 large-4 end">
       <Link className="prompts-tile" to={`/prompts/${props.id}`}>
-          <div className="content">
-          <h4 className="name">User ID: {props.author}</h4>
-          <p className="description">
+        <div className="content">
+          <h3 className="description">
             {props.description}
-          </p>
+          </h3>
+          <h6 className="name">User ID: {props.author}</h6>
         </div>
       </Link>
     </div>
