956675309617bca54186f3e66a041cb7462dd45f
ChoiChips <christianchoi2@gmail.com>
2018-05-09 17:42:14 -0400

    -Minor change to prompt_spec

--- spec/models/prompt_spec.rb
+++ spec/models/prompt_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Prompt do
-  test_user = User.create!(email: "prompt_model@gmail.com", password: "password", password_confirmation: "password")
+  test_user = User.create!(handle: "test_user", email: "prompt_model@gmail.com", password: "password", password_confirmation: "password")
   test_prompt = Prompt.new(description: "This is test prompt 1, written by user_1, to be answered with a test response.", user: test_user)
 
   it "is valid with valid attributes" do
