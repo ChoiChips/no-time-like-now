842775608b4a3996d4fbbcf8093126aa9aa30f86
ChoiChips <christianchoi2@gmail.com>
2018-06-11 14:54:48 -0400

    -Removed old comments

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,21 +1,3 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate_user!
-
-# consider moving to either API controller or application controller?
-  # private
-  # def recent_answer
-  #   if user_signed_in?
-  #     most_recent_answer = Answer.where(user: current_user).order('created_at DESC').last
-  #     if most_recent_answer.nil?
-  #       # infinite loop, consider having additional condition checking path
-  #       random_prompt = Prompt.all.sample.id
-  #       redirect_to '/prompts/#{random_prompt}/new', notice: "Please submit a response"
-  #     elsif (most_recent_answer.created_at + 1.day) < Time.current
-  #       random_prompt = Prompt.all.sample.id
-  #       redirect_to '/prompts/#{random_prompt}/new', notice: "Please submit a response"
-  #     end
-  #   else
-  #     redirect_to "/users/sign_in", notice: "Please sign in"
-  #   end
-  # end
 end
