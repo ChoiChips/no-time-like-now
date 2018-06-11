3986f5cbc4ae9bad01e29f6db091348d02edb217
ChoiChips <christianchoi2@gmail.com>
2018-06-11 14:57:51 -0400

    -Removed old comments

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -28,26 +28,3 @@ class Api::V1::AnswersController < ApiController
     params.require(:answer).permit(:answer)
   end
 end
-
-# notes for checking if user has posted in past 24 hours
-# beforeaction
-# current user
-# if thye posted
-#   to prevent laoding wrong page need skip_before_action
-#
-# react router listen for routing, hijack event and check if user has posted
-#   if so, allow route
-#     if not redirect to post
-#
-# for json endpoints display 401 unauth or 403 if user has not posted.
-#
-#
-# want layout component with children. Then in that componentWillReceiveProps will check to see
-# if there is a current user and if current user has posted and not on post page, then will route them to post page.
-#   In component will receive props will check children and change chilren and route accordingly
-#   In component will mount will redirect using routing features to posting page if they have to post, otherwise let route process and
-#   not change children.
-#
-# Browserhistory.push
-#
-# window.onfocus and onblur
